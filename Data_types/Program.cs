d14fdf7776a6604eaa27e841e63275868fe9bed2
Evgeny40rus <hat-99@mail.ru>
2021-03-07 16:30:27 +0300

    Создание диалога

--- Data_types/Program.cs
+++ Data_types/Program.cs
@@ -2,11 +2,19 @@
 
 namespace Data_types
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+    class MainClass {
+        public static void Main(string[] args) {
+            string Myname = "Евгений";
+            byte MyAge = 27;
+            bool HaveIApet = true;
+            double MyShoeSize = 37.5;
+
+            Console.WriteLine("Как вас зовут" + Myname);
+            Console.WriteLine("Сколько вам лет" + MyAge);
+            Console.WriteLine("Если ли у вас питомец?" + HaveIApet);
+            Console.WriteLine("Какой у вас размер ноги?" + MyShoeSize);
         }
     }
-}
+
+
+    
