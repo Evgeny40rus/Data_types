c000679c3f4fe409ea4900a68a7c31cc606210ac
Evgeny40rus <hat-99@mail.ru>
2021-03-07 16:48:22 +0300

    Незначительные изменения кода

--- Data_types/Program.cs
+++ Data_types/Program.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 
-namespace Data_types
-{
-    class MainClass {
-        public static void Main(string[] args) {
-            string Myname = "Евгений";
-            byte MyAge = 27;
-            bool HaveIApet = true;
-            double MyShoeSize = 37.5;
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string Myname =  "Евгений";
+            byte MyAge =  27;
+            bool HaveIApet =  true;
+            double MyShoeSize =  37.5;
 
-            Console.WriteLine("Как вас зовут" + Myname);
-            Console.WriteLine("Сколько вам лет" + MyAge);
-            Console.WriteLine("Если ли у вас питомец?" + HaveIApet);
-            Console.WriteLine("Какой у вас размер ноги?" + MyShoeSize);
+            Console.WriteLine("Как вас зовут" +  Myname);
+            Console.WriteLine("Сколько вам лет" +  MyAge);
+            Console.WriteLine("Если ли у вас питомец?" +  HaveIApet);
+            Console.WriteLine("Какой у вас размер ноги?" +  MyShoeSize);
         }
     }
 
